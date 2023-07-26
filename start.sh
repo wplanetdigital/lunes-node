21b668080a75775bcb56f23bf91205cc9af806cb
wplanetdigital <91206900+wplanetdigital@users.noreply.github.com>
2023-07-26 12:45:53 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,14 +1,11 @@
 #!/bin/bash
 
+sudo apt update
 sudo apt install build-essential
-sudo apt install --assume-yes git clang curl libssl-dev llvm libudev-dev make protobuf-compiler
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+sudo apt install --assume-yes git clang curl libssl-dev protobuf-compiler
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
 source $HOME/.cargo/env
-rustup default stable
-rustup update
-rustup update nightly
-rustup target add wasm32-unknown-unknown --toolchain nightly
 rustup default nightly-2023-01-01
-rustup target add wasm32-unknown-unknown
+rustup target add wasm32-unknown-unknown --toolchain nightly-2023-01-01
+cd lunes-node
 cargo build --release
-./target/release/lunes-node --dev
